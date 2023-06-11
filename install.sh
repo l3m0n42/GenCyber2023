c6bba76de47097186274cc0b129b43b2003a9b03
Alberto Yerena <101772709+l3m0n42@users.noreply.github.com>
2023-06-11 02:05:07 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]; then
+        echo 'script requires root privileges'
+        exit 1
+package_install(){
+  #installing metasploit 
+  apt update
+  apt install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev openjdk-11-jdk git autoconf postgresql pgadmin3 zlib1g-dev libxml2-dev libxslt1-dev libyaml-dev curl ruby-dev nmap -y
+  cd /usr/local/bin
+  git clone https://github.com/rapid7/metasploit-framework.git
+  cd metasploit-framework
+  sudo gem install bundler
+  bundle install
+  sudo service postgresql start
+  sudo msfdb init
+  sudo msfdb start
 
+}
