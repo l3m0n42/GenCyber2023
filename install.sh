200e8afd1b563a45e9555a9960cd2184a6aeb80a
Alberto Yerena <101772709+l3m0n42@users.noreply.github.com>
2023-06-12 11:29:37 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,21 +17,30 @@ package_install(){
       	apt install nmap tcpdump iputils-ping netdiscover openssh-server -y
       # Installing Docker
      	apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-	cd /home/
+	mkdir /home/pi/cyber-analyst
+	cd /home/pi/cyber-analyst
 	wget https://raw.githubusercontent.com/l3m0n42/GenCyber2023/main/docker-compose.yaml
 	wget https://raw.githubusercontent.com/l3m0n42/GenCyber2023/main/zapfix.sh
+      #Installing Forensics
+      	mkdir /home/pi/forensics
+	cd /home/pi/forensics
+      	wget https://github.com/syvaidya/openstego/releases/download/openstego-0.8.6/openstego_0.8.6-1_all.deb
+	apt install openjdk-8-jre
+	dpkg -i openstego_0.8.6-1_all.deb
+	wget https://file.io/PYFpmdccaLbX
+	unzip Embedded.zip
 
 }
 adding_users(){
       # Create three users
-	useradd -m student1
-	useradd -m student2
-	useradd -m student3
+	useradd -m camper1
+	useradd -m camper2
+	useradd -m camper3
 
       # Set passwords for the users
-	echo "student1:gencyber" | chpasswd
-	echo "student2:gencyber" | chpasswd
-	echo "student3:gencyber" | chpasswd
+	echo "camper1:gencyber" | chpasswd
+	echo "camper2:gencyber" | chpasswd
+	echo "camper3:gencyber" | chpasswd
 }
 adding_users
 package_install
