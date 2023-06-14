8a4baa27a82173c75006de7a15407f926601380a
Alberto Yerena <101772709+l3m0n42@users.noreply.github.com>
2023-06-14 09:29:24 -0700

    Update vasinstall.sh

--- vasinstall.sh
+++ vasinstall.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-#check for root
-if [ "$EUID" -ne 0 ]; then
-        echo 'script requires root privileges'
-        exit 1
-fi
 vas_installer(){
-  apt update
-  pip3 install docker-compsoe
+  sudo apt update
+  sudo pip3 install docker-compsoe
   mkdir /home/pi/VulnAnalysis
   cd /home/pi/VulnAnalysis
   apt install curl -y
   curl -f -O https://raw.githubusercontent.com/l3m0n42/GenCyber2023/main/setup-and-start-greenbone-community-edition.sh && chmod u+x setup-and-start-greenbone-community-edition.sh
-  ./setup-and-start-greenbone-community-edition.sh 22.4
+  sudo ./setup-and-start-greenbone-community-edition.sh 22.4
 }
 vas_installer
 echo "Navigate to 127.0.0.1:9392"
